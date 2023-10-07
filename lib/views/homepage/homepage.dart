a7880d17a6273cc90749636169c675f2b83a9e63
Akashi <mohitsilwal16@gmail.com>
2023-10-07 18:38:28 +0530

    Home Page starts
    Signed-off-by: Akashi <mohitsilwal16@gmail.com>

--- lib/views/homepage/homepage.dart
+++ lib/views/homepage/homepage.dart
@@ -8,21 +8,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
       ),
+      body: index == 0
+          ? const SizedBox(
+              child: Text('Profile'),
+            )
+          : index == 1
+              ? Container(
+                  child: const Text('Cart'),
+                )
+              : index == 2
+                  ? Container(
+                      child: const Text('Home'),
+                    )
+                  : Container(child: const Text('Message')),
       bottomNavigationBar: BottomNavigationBar(
         items: const [
           BottomNavigationBarItem(
+            label: 'Profile',
+            icon: Icon(Icons.person_2_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: 'Cart',
+            icon: Icon(Icons.shopping_cart_outlined),
+          ),
+          BottomNavigationBarItem(
+            label: 'Home',
             icon: Icon(Icons.home),
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.card_travel),
-          )
+          ),
+          BottomNavigationBarItem(
+            label: 'Chat',
+            icon: Icon(Icons.message_outlined),
+          ),
         ],
+        onTap: (int ind) {
+          setState(() {
+            index = ind;
+          });
+        },
       ),
     );
   }
