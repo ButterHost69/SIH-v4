04eb73ddd3e31b905f4ba4df7de3abb806993b52
Akashi <mohitsilwal16@gmail.com>
2023-10-08 09:36:53 +0530

    Navigation Main Page added
    Signed-off-by: Akashi <mohitsilwal16@gmail.com>

--- lib/login.dart
+++ lib/login.dart
@@ -18,63 +18,58 @@ class _LoginState extends State<Login> {
           child: Container(
             margin: const EdgeInsets.symmetric(horizontal: 30),
             child: SizedBox(
-      
               child: Column(
                 children: [
-      
-                    const Text(
-                      'Ecom Mart',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 38,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  const Text(
+                    'Ecom Mart',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 38,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Text(
-                      'Small Business, Big Impact',
-                      style: TextStyle(
-                        color: Colors.blue,
-                        fontSize: 14,
-                      ),
+                  ),
+                  const Text(
+                    'Small Business, Big Impact',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 14,
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Image.asset('assets/images/applogo.png'),
-                    const SizedBox(
-                      height: 20,
-                    ),
-      
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Image.asset('assets/images/applogo.png'),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   TextField(
                     decoration: InputDecoration(
                       hintText: 'Email',
                       border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide: const BorderSide(
-                              color: Colors.blue, // Set border color
-                              width: 2.0, // Set border width
-                            ),
-                          ),
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue, // Set border color
+                          width: 2.0, // Set border width
+                        ),
+                      ),
                     ),
                     onChanged: (value) {
-                      email=value;
+                      email = value;
                     },
                   ),
-
                   const SizedBox(
                     height: 10,
                   ),
-
                   TextField(
                     decoration: InputDecoration(
                       hintText: 'Enter Password Here',
                       border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                            borderSide: const BorderSide(
-                              color: Colors.blue, // Set border color
-                              width: 2.0, // Set border width
-                            ),
-                          ),
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(
+                          color: Colors.blue, // Set border color
+                          width: 2.0, // Set border width
+                        ),
+                      ),
                     ),
                     onChanged: (value) {
                       pass = value;
@@ -87,24 +82,24 @@ class _LoginState extends State<Login> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-          
                       // Login Button
                       ElevatedButton(
-                       style: ButtonStyle(
-                              padding: MaterialStateProperty.all(
-                                const EdgeInsets.symmetric(horizontal: 35),
-                              ),
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                              ),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.blue),
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(horizontal: 35),
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
                             ),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.blue),
+                        ),
                         onPressed: () async {
                           try {
-                            UserCredential userCredential = await FirebaseAuth.instance
+                            UserCredential userCredential = await FirebaseAuth
+                                .instance
                                 .signInWithEmailAndPassword(
                                     email: email, password: pass);
                             Navigator.pushNamed(context, 'home');
@@ -112,39 +107,38 @@ class _LoginState extends State<Login> {
                             if (e.code == 'user-not-found') {
                               print('No users Found for that email.');
                             } else if (e.code == 'wrong-password') {
-                              print('Incorrect password provided for that user.');
+                              print(
+                                  'Incorrect password provided for that user.');
                             }
                           }
                         },
                         child: const Text('Login'),
                       ),
-                    
-                    
+
                       //Register Button
-                       OutlinedButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, 'register');
-                            },
-                            style: ButtonStyle(
-                              padding: MaterialStateProperty.all(
-                                const EdgeInsets.symmetric(horizontal: 35),
-                              ),
-                              shape: MaterialStateProperty.all(
-                                RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                ),
-                              ),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.white),
-                            ),
-                            child: const Text("Register"),
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, 'register');
+                        },
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                            const EdgeInsets.symmetric(horizontal: 35),
                           ),
-          
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
+                            ),
+                          ),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.white),
+                        ),
+                        child: const Text("Register"),
+                      ),
                     ],
                   ),
                   const SizedBox(
                     height: 20,
-                  ),  
+                  ),
                 ],
               ),
             ),
